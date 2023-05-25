6b76428b281ff1f16f185a2fd7f4a95576dc48fd
MW1R1 <nickmwirigi58@gmail.com>
2023-05-25 14:59:41 +0300

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,25 @@
 # your code goes here
+class Person
+  attr_reader :name
+  attr_accessor :bank_account, :happiness, :hygiene
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def bank_account=(value)
+    @bank_account = value
+  end
+
+  def happiness=(value)
+    @happiness = value.clamp(0, 10)
+  end
+
+  def hygiene=(value)
+    @hygiene = value.clamp(0, 10)
+  end
+end
+
